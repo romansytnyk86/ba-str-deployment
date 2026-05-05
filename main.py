5050bcb66b4c761611823d3ff75f9a375d0357f1
Roman Sytnyk <rsytnyk@strategy.com>
2026-05-05 12:49:30 +0200

    deleted:    duplicate_project_test.py deleted:    files/config.py deleted:    files/main.py deleted:    files/mstr/__init__.py deleted:    files/mstr/connection.py deleted:    files/mstr/dbconnection.py deleted:    files/mstr/duplicate.py deleted:    files/mstr/project.py deleted:    files/mstr/schema.py deleted:    files/mstr/security.py deleted:    files/plan_template.py deleted:    files/routing.py deleted:    files/utils/__init__.py deleted:    files/utils/logger.py deleted:    files/workflows/__init__.py deleted:    files/workflows/deploy_with_backup_duplicate.py deleted:    files/workflows/deploy_with_backup_merge.py deleted:    files/workflows/deploy_without_backup.py deleted:    main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Top-level CLI wrapper for the SGB II MaEnde deployment tool."""
-
-from files.main import main
-import sys
-
-if __name__ == "__main__":
-    sys.exit(main())
